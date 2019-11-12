35ffbb1e23df12dbbbbb1ae6ae5735cb7d5caba4
sravanthy09 <dsravanthy9@gmail.com>
2019-11-11 21:13:29 -0600

    Update test1.groovy

--- vars/test1.groovy
+++ vars/test1.groovy
@@ -1,1 +1,9 @@
 
+import groovy.grape.Grape
+@Grab(group ='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.5.2')
+
+
+import groovyx.net.http.*
+import groovyx.net.http.ContentType.*
+import groovyx.net.http.Method.*
+import net.sf.json.*
