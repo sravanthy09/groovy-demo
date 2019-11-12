3448eb89043d50ed5ac640a8b5865ea620a4111c
sravanthy09 <dsravanthy9@gmail.com>
2019-11-11 21:57:33 -0600

    Update test1.groovy

--- vars/test1.groovy
+++ vars/test1.groovy
@@ -1,9 +1,15 @@
 #!groovy
-import groovy.grape.Grape
-@Grab(group ='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.5.2')
 
 
-import groovyx.net.http.*
-import groovyx.net.http.ContentType.*
-import groovyx.net.http.Method.*
-import net.sf.json.*
+
+def lib(){
+    import groovy.grape.Grape
+    @Grab(group ='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.5.2')
+
+
+    import groovyx.net.http.*
+    import groovyx.net.http.ContentType.*
+    import groovyx.net.http.Method.*
+    import net.sf.json.*
+
+}
